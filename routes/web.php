e25f04fa2401ead84371b029c532c0621ec97a7b
arafi118 <arp46301@gmail.com>
2024-06-13 13:45:29 +0700

    Siupk

--- routes/web.php
+++ routes/web.php
@@ -269,8 +269,8 @@ Route::resource('/profil', UserController::class);
 
 Route::get('/sync/{lokasi}', [DashboardController::class, 'sync'])->middleware('auth');
 Route::get('/link', function () {
-    $target = '/home/dbm/public_html/simak/storage/app/public';
-    $shortcut = '/home/dbm/public_html/simak/public/storage';
+    $target = '/home/siupk/public_html/simak/storage/app/public';
+    $shortcut = '/home/siupk/public_html/simak/public/storage';
     symlink($target, $shortcut);
 });
 
